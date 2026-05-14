3f2b349a8f77a632af23180bb3375bc969d12c6a
NaveenN267 <naveen.acc24@gmail.com>
2026-05-14 22:25:01 +0530

    Update files

---
+++ chapter_04_Variables/08_Var_Varibale.js
@@ -0,0 +1,19 @@
+var browser = "Chrome";
+var browser = "Firefox";
+console.log(browser);
+
+for (var i = 0; i < 10; i++) {
+    //console.log(i);
+}
+
+function name() {
+    console.log("Namala Naveen");
+}
+name();
+function vari() {
+    var browser = ["Edge", "Opera"];
+    for (var i = 0; i < browser.length; i++) {
+        //console.log(i);
+    } console.log(i);
+}
+vari();
---
+++ chapter_04_Variables/09_Fucntion.js
@@ -0,0 +1,10 @@
+function name() {
+    console.log("Namala Naveen");
+}
+name();
+function add() {
+    var a = 10;
+    var b = 20;
+    console.log(a + b);
+}
+add();
